79820574d2646ff373875e14672044d5047c1b9d
Meejach <deax0123@stud.kea.dk>
2022-11-14 09:36:24 +0100

    Animal sounds

---
+++ src/main/java/main.java
@@ -0,0 +1,14 @@
+public class main {
+    public static void main(String[] args) {
+
+        Animal animal = new Dog();
+        Animal animal1 = new Cat();
+
+        animal.makeSound();
+        animal1.makeSound();
+
+    }
+
+
+
+}
